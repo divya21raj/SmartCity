0c37014055db38bb55c3fbf83d5f4b2d2a77c102
Divya Raj <divya21raj@gmail.com>
2017-10-23 21:42:44 +0530

    Funny Drone developed

--- src/com/javalab/Drones/FunnyDrone.java
+++ src/com/javalab/Drones/FunnyDrone.java
@@ -2,13 +2,67 @@ package com.javalab.Drones;
 
 import com.javalab.Drone;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class FunnyDrone extends Drone
 {
+    private Scanner scanner = new Scanner(System.in);
+
+    private static ArrayList<ArrayList<String>> jokes = new ArrayList<>();
 
     @Override
-    protected void interact()
+    protected void interact() throws IOException
     {
+        System.out.println("Hey! My name is Funny Drone " + this.getId());
 
+        printRandomJoke();
+    }
+
+    private void printRandomJoke()
+    {
+        Random random = new Random();
+
+        int jokeIndex = random.nextInt(jokes.size());
+
+        System.out.println("Here's a rather funny joke...");
+        scanner.nextLine();
+
+        for (String line: jokes.get(jokeIndex))
+            System.out.println(line);
+
+        System.out.printf("\n:);):);):);):);):);):);)See ya!");
+        scanner.nextLine();
+    }
+
+    public FunnyDrone() throws IOException
+    {
+        if(jokes.isEmpty())
+        {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("files/Jokes/jokes.txt"));
+
+            String line;
+            ArrayList<String> joke = new ArrayList<>();
+
+            while(true)
+            {
+                line = bufferedReader.readLine();
+
+                if(line == null)
+                    break;
+
+                if(line.equals(""))
+                {
+                    jokes.add(joke);
+                    joke = new ArrayList<>();
+                }
+                else
+                    joke.add(line);
+
+            }
+        }
     }
 
     @Override
